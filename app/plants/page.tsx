c076e8467ddbd0b59e9ddacc28dd79fdf12def51
Zach Stout <stout.zachary@gmail.com>
2024-02-15 09:24:24 -0700

    added in carousel, rendering in plants page

--- app/plants/page.tsx
+++ app/plants/page.tsx
@@ -1,8 +1,11 @@
+import { PlantCarousel } from "@/components/plant-carousel";
+
 export default function Plants() {
   return (
-    <div>
+    <div className="container">
       <h1>Plants</h1>
       <p>Learn about the different types of plants.</p>
+      <PlantCarousel />
     </div>
   );
 }
