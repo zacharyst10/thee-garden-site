d9c4f938ed8db85d216bcfb13451e3ccaed77bca
Zach Stout <stout.zachary@gmail.com>
2024-02-15 12:26:45 -0700

    added in drawer and got the data to the drawer

--- app/plants/page.tsx
+++ app/plants/page.tsx
@@ -1,10 +1,11 @@
+import PlantDrawer from "@/components/drawer";
 import { PlantCarousel } from "@/components/plant-carousel";
 
 export default function Plants() {
   return (
     <div className="container">
       <h1>Plants</h1>
-      <p>Learn about the different types of plants.</p>
+      <p>Learn about the different types of plants or something!</p>
       <PlantCarousel />
     </div>
   );
--- components/plant-carousel.tsx
+++ components/plant-carousel.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -15,7 +15,45 @@ import pepper from "@/public/plants/pepper.jpg";
 import tomato from "@/public/plants/tomato.jpg";
 import raspberry from "@/public/plants/raspberry.jpg";
 
-const images = [blueberry, onion, pepper, tomato, raspberry];
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "./ui/button";
+
+export default function PlantDrawer() {
+  return (
+    <Drawer>
+      <DrawerTrigger>Open</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+const images = [
+  { image: blueberry, title: "Blueberry" },
+  { image: onion, title: "Onion" },
+  { image: pepper, title: "Pepper" },
+  { image: tomato, title: "Tomato" },
+  { image: raspberry, title: "Raspberry" },
+];
 
 export function PlantCarousel() {
   return (
@@ -28,23 +66,40 @@ export function PlantCarousel() {
     >
       <CarouselContent>
         {images.map((image, index) => (
-          <CarouselItem
-            key={index}
-            className="md:basis-1/2 lg:basis-1/4 rounded-lg"
-          >
-            <div className="p-1">
-              <Card className="p-0">
-                <CardContent className="flex p-0 aspect-square items-center rounded-lg justify-center w-full h-auto">
-                  <Image
-                    src={image}
-                    alt="Plant"
-                    className="object-cover w-full h-full rounded-lg"
-                    placeholder="blur"
-                  />
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
+          <Drawer key={index}>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/4 rounded-lg">
+              <div className="p-1">
+                <DrawerTrigger>
+                  <Card className="p-0">
+                    <CardContent className="flex p-0 aspect-square items-center rounded-lg justify-center w-full h-auto">
+                      <Image
+                        src={image.image}
+                        alt="Plant"
+                        className="object-cover w-full h-full rounded-lg"
+                        placeholder="blur"
+                      />
+                    </CardContent>
+
+                    <CardFooter className="flex justify-center text-5xl pt-5 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">
+                      {image.title}
+                    </CardFooter>
+                  </Card>
+                </DrawerTrigger>
+              </div>
+            </CarouselItem>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>{image.title}</DrawerTitle>
+                <DrawerDescription>Add to garden</DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
         ))}
       </CarouselContent>
       <CarouselPrevious />
