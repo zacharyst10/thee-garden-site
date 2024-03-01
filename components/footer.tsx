73e872270e47a81eda4706936174fb49c3f1412f
Zach Stout <stout.zachary@gmail.com>
2024-03-01 12:45:28 -0700

    poopy footer not poopy no more

--- components/footer.tsx
+++ components/footer.tsx
@@ -69,7 +69,7 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="bg-muted" aria-labelledby="footer-heading">
+    <footer className="bg-muted " aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
