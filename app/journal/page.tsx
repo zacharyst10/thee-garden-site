f1d9dbdb099cfdc06694ee20194a1c96a172c1df
Zach Stout <stout.zachary@gmail.com>
2024-04-06 22:20:48 -0600

    ya know

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -165,7 +165,7 @@ export default function Dashboard() {
           fallback={
             <div className="text-center">
               <LoaderPage />
-              Loading Order Details...
+              Loading...
             </div>
           }
         >
